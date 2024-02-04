3a7f110a03a0c30735b2c08f7c1323aacf10d5c2
Soham More <141304014+SohamMore100@users.noreply.github.com>
2024-02-04 11:29:51 +0530

    Js Code Updated

--- qrcode.js
+++ qrcode.js
@@ -1,19 +1,41 @@
-const qrinput = document.querySelector('qr-input')
-const qrimg = document.querySelector('qr-img')
-const qrbutton = document.querySelector('qr-button')
+// const qrinput = document.querySelector('qr-input')
+// const qrimg = document.querySelector('qr-img')
+// const qrbutton = document.querySelector('qr-button')
 
-// remove this for Github
+// // remove this for Github
 // console.log(qrbutton,qrimg,qrinput)
 
-qrbutton.addEventListener('click',()=>{
-    const inputValue = qrinput.value
-    console.log(inputValue)
+// qrbutton.addEventListener('click',()=>{
+//     const inputValue = qrinput.value
+//     console.log(inputValue)
 
-    if (!inputValue) {
+//     if (inputValue== '') {
+//         alert('Please enter a valid URL');
+//         return;
+//     } else {
+//         qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;;
+//         qrimg.alt = `QR code for ${inputValue}`;
+//     }
+
+// })
+
+const qrinput = document.getElementById('qr-input');
+const qrimg = document.getElementById('qr-img');
+const qrbutton = document.getElementById('qr-button');
+
+console.log(qrinput, qrimg, qrbutton)
+
+qrbutton.addEventListener('click', () => {
+
+    const inputValue = qrinput.value;
+    console.log(inputValue)    
+
+    if( !inputValue ) {
         alert('Please enter a valid URL');
         return;
-    } else {
-        qrimg.src = '';
-        qrimg.alt = '';
+    }else{
+        qrimg.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+        qrimg.alt = `QR code for ${inputValue}`;
     }
-})
+
+});
