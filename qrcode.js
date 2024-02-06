cfab4a9e1081901325989e55a170952251c7a6e3
Soham More <141304014+SohamMore100@users.noreply.github.com>
2024-02-06 21:50:00 +0530

    Update qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -1,23 +1,3 @@
-// const qrinput = document.querySelector('qr-input')
-// const qrimg = document.querySelector('qr-img')
-// const qrbutton = document.querySelector('qr-button')
-
-// // remove this for Github
-// console.log(qrbutton,qrimg,qrinput)
-
-// qrbutton.addEventListener('click',()=>{
-//     const inputValue = qrinput.value
-//     console.log(inputValue)
-
-//     if (inputValue== '') {
-//         alert('Please enter a valid URL');
-//         return;
-//     } else {
-//         qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;;
-//         qrimg.alt = `QR code for ${inputValue}`;
-//     }
-
-// })
 
 const qrinput = document.getElementById('qr-input');
 const qrimg = document.getElementById('qr-img');
